26b2f5f9b6800b360f76a180ec4a1c5e3989e967
Thais Bauerfeld <bauerfeldthais@gmail.com>
2022-10-03 21:21:02 +0100

    Class

---
+++ Class.js
@@ -0,0 +1,18 @@
+Class Student{
+	//nome, idade, gênero, ano,
+	name;
+	age;
+	gender;
+	grade;
+
+	//
+	school subjects();
+	grade();
+
+function sumGrade(grade){
+	let result = 0
+	for (let i = 0; i < grade.lenght; i++){
+		result += [i]
+	}
+	return result / grade.lenght
+}
